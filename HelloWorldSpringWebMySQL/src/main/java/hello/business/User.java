7de158bc0c332ee37813a5285186e7bbbcc6ede5
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-01-26 13:08:59 -0500

    Finish All User CRUD methods in Spring

--- HelloWorldSpringWebMySQL/src/main/java/hello/business/User.java
+++ HelloWorldSpringWebMySQL/src/main/java/hello/business/User.java
@@ -1,4 +1,4 @@
-package hello;
+package hello.business;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- HelloWorldSpringWebMySQL/src/main/java/hello/business/UserRepository.java
+++ HelloWorldSpringWebMySQL/src/main/java/hello/business/UserRepository.java
@@ -1,4 +1,4 @@
-package hello;
+package hello.business;
 
 import org.springframework.data.repository.CrudRepository;
 
