ae6a6169c3b8bd8d4ef415e475b1c36a8ca11d8f
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-02-04 00:02:31 -0500

    Finished CRUD functions for all Controllers

--- bmdbSpringProject/src/main/java/com/bmdb/business/MovieRepository.java
+++ bmdbSpringProject/src/main/java/com/bmdb/business/MovieRepository.java
@@ -1,9 +1,11 @@
 package com.bmdb.business;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface MovieRepository extends PagingAndSortingRepository<Movie, Integer> { // <Object, Data Type>
 	// This will be AUTO IMPLEMENTED by Spring into a Bean called genreRepository
 	// CRUD refers Create, Read, Update, Delete
-	Movie findByRating(String rating);
+	List <Movie> findByRating(String rating);
 }
